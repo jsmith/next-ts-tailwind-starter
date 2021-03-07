576615e9c94aa68505a0fc4e5c354ac1ce819ef3
Jacob Smith <jacob.smith@unb.ca>
2021-03-07 14:39:44 -0400

    added tailwind + typescript

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <main className="font-bold text-center">Hello!</main>
+
+      <footer>{/* TODO */}</footer>
+    </div>
+  );
+}
